e403e3efce60b83a36f19ea5733c3e6c408d1ed0
Mariia Bulchak <bulchakmariiast@gmail.com>
2025-03-14 10:10:07 +0200

    fixing header people class

--- src/components/HeaderPeople/HeaderPeople.tsx
+++ src/components/HeaderPeople/HeaderPeople.tsx
@@ -21,32 +21,32 @@ export const HeaderPeople: React.FC<Props> = ( {
   return (
     <div className='header__people'>
       <div className='header__person--1'>
-        <img className='header__person first__person' src={person_1} alt='first person' />
+        <img className='header__person' src={person_1} alt='first person' />
         <div className='header__person__effect'></div>
       </div>
 
       <div className='header__person--2'>
-        <img className='header__person first__person' src={person_2} alt='first person' />
+        <img className='header__person' src={person_2} alt='first person' />
         <div className='header__person__effect'></div>
       </div>
 
       <div className='header__person--3'>
-        <img className='header__person first__person' src={person_3} alt='first person' />
+        <img className='header__person' src={person_3} alt='first person' />
         <div className='header__person__effect'></div>
       </div>
 
       <div className='header__person--4'>
-        <img className='header__person first__person' src={person_4} alt='first person' />
+        <img className='header__person' src={person_4} alt='first person' />
         <div className='header__person__effect'></div>
       </div>
 
       <div className='header__person--5'>
-        <img className='header__person first__person' src={person_5} alt='first person' />
+        <img className='header__person' src={person_5} alt='first person' />
         <div className='header__person__effect'></div>
       </div>
 
       <div className='header__person--6'>
-        <img className='header__person first__person' src={person_6} alt='first person' />
+        <img className='header__person' src={person_6} alt='first person' />
         <div className='header__person__effect'></div>
       </div>
     </div>
