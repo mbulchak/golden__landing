22d954ea5bbe1660fd8de588e267a4e57de17bc1
Mariia Bulchak <bulchakmariiast@gmail.com>
2025-03-15 12:32:11 +0200

    add functionallity for team block while scrolling

--- src/widgets/Team/Team.tsx
+++ src/widgets/Team/Team.tsx
@@ -2,12 +2,53 @@ import {Button} from '../../components/Button';
 import './Team.scss';
 
 import bitcoins from './../../assets/images/coins/lotBitcoin.png';
-import team1 from './../../assets/images/team/team-1.png'
+import team1 from './../../assets/images/team/team-1.png';
+import team2 from './../../assets/images/team/team-2.png';
+import team3 from './../../assets/images/team/team-3.png';
+
+import {gsap} from 'gsap';
+import {useGSAP} from '@gsap/react';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import {TextPlugin} from 'gsap/TextPlugin';
+import { useRef } from 'react';
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, TextPlugin);
 
 export const Team = () => {
+  const container = useRef(null);
+
+  console.log(container)
+
+  useGSAP(() => {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: container.current,
+        start: 'center center',
+        end: '+=1000',
+        scrub: 1,
+        pin: true,
+      },
+    });
+
+    tl.set('.team__member', {attr: {src: team1}})
+      .to('.team__member', {opacity: 0, duration: 0.5})
+      .set('.team__member', {attr: {src: team2}})
+      .to('.team__member', {opacity: 1, duration: 0.5})
+      .to('.team__title', {duration: 1, text: 'Member 2 of our expert team'})
+      .to('.team__description', {
+        duration: 1,
+        text: 'Lörem ipsum fulparkerare krollipsum diska didebelt respektive spevis plus krofenatipsum',
+      })
+      .to('.team__member', {opacity: 0, duration: 0.5})
+      .set('.team__member', {attr: {src: team3}})
+      .to('.team__title', {duration: 0, text: 'Member 3 of our expert team'})
+      .to('.team__description', {duration: 0, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo ipsum aliquet turpis elit elit natoque varius eget facilisi. Amet rhoncus'})
+      .to('.team__member', {opacity: 1, duration: 0.3});
+  }, { scope: container });
+
   return (
     <>
-      <div className='team'>
+      <div className='team' ref={container}>
         <div className='team__content'>
           <h5 className='team__sub--title'>Team</h5>
 
